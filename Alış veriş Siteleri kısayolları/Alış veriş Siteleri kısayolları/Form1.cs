abeaf8a270c1c533b879f1441f40d329b1a61303
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 20:48:01 +0300

    Add files via upload

---
+++ Alış veriş Siteleri kısayolları/Alış veriş Siteleri kısayolları/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.teknosa.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "TEKNOSA";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.n11.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "n.11";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.hepsiburada.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Hepsi Burada";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.aliexpress.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Ali Express";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.gittigidiyor.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Gitti Gidiyor";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://www.trendyol.com");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Trendyol";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.markafoni.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Markofoni";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://www.morhipo.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Morhibo";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.boyner.com.tr/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Boyner";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://www.ykm.com.tr/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Ykm.com.tr";
+        }
+    }
+}
---
+++ Alış veriş Siteleri kısayolları/Alış veriş Siteleri kısayolları/Form2.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            resources.ApplyResources(this.webBrowser1, "webBrowser1");
+            this.webBrowser1.Name = "webBrowser1";
+            // 
+            // Form2
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.WebBrowser webBrowser1;
+
+    }
+}
